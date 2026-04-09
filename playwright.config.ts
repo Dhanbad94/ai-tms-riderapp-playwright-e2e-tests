6750092487842f5454245163110033396d4d8628
Abhishek <abhishek.dubey@appinventiv.com>
2026-04-09 20:52:27 +0530

    Merge remote main with local code

--- playwright.config.ts
+++ playwright.config.ts
@@ -49,7 +49,7 @@ export default defineConfig({
     trace: "retain-on-failure",
     screenshot: "only-on-failure",
     video: "retain-on-failure",
-    headless: false,
+    headless: true,
     ignoreHTTPSErrors: true,
     actionTimeout: TIMEOUTS.ACTION,
     navigationTimeout: TIMEOUTS.NAVIGATION,
